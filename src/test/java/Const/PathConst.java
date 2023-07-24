42cef537a373f62790e16fc0f2b05e65f0b0dcc5
Romans66 <Frolenkov_60@mail.ru>
2023-07-24 21:34:34 +0300

    new

--- src/test/java/Const/PathConst.java
+++ src/test/java/Const/PathConst.java
@@ -5,5 +5,4 @@ public interface PathConst {
   String PATHSPECPWD = ".\\src\\test\\resources\\Компании-спецпароль.txt";
   String PATHINNKK = ".\\src\\test\\resources\\Компании — ИНН + КК.txt";
   String PATHVIPAUTHOL = ".\\src\\test\\resources\\Тестовые логины VIP пользователей + пароли (Ответственные лица).txt";
-
 }
